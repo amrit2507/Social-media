5c071a1b8fb874b65182595d7a8f866f3e6aa77d
Amritanshu Sharma <amritanshusharma@Amritanshus-MacBook-Air.local>
2021-05-28 20:07:03 +0530

    message

--- config/environment.js
+++ config/environment.js
@@ -23,7 +23,7 @@ const development = {
         secure: false,
         auth:{
             user:'amrit2000000@gmail.com',
-            pass:'Qwerty@2000'
+            pass:''
         }
     },
     google_client_id: "744770314086-6g64rjtb5l4n333943pnemdp4rptgisl.apps.googleusercontent.com",
